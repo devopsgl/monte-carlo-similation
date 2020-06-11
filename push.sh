5340c60b6914e64561cd3b316eb5a63cb57cb142
burak <burakgul01@gmail.com>
2020-06-11 17:22:49 +0300

    123

--- push.sh
+++ push.sh
@@ -4,9 +4,4 @@ read -p 'commit girin: ' commit
 git add .
 git commit -m "$commit"
 git push origin master
-expect "Username for 'https://github.com'"
-send "burakgul01\r"
-echo "password"
-send "Burak137."
-echo ""
-
+exec echo "burakgul01"
